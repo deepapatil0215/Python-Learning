edd718598e4577de55d52c44a1156aabb2d470b0
shivanand Kadakal <shivanand.kadakal@gmail.com>
2025-01-08 09:07:46 +0530

    studentdetails

---
+++ SD.py
@@ -0,0 +1,19 @@
+#program for student details
+
+sd1id = 100
+sd1na = "john"
+sd1ph = 9875641234
+sd1add = "OMR thoraipakkam, chennai"
+
+
+sd2id = 101
+sd2na = "smith"
+sd2ph = 8976123456
+sd2add = "besantNagar, chennai"
+
+
+print(str(sd1id)+sd1na+str(sd1ph)+sd1add)
+print(str(sd2id)+sd2na+str(sd2ph)+sd2add)
+
+
+
